fc0c267661710eacdcd6b88e14e0df0c0e0a1e46
Cameron Murphy <cameron.murphy@catchoftheday.com.au>
2012-09-13 22:23:50 +1000

    Completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Camurphy.CenturionCounter
 {
@@ -20,17 +21,48 @@ namespace Camurphy.CenturionCounter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int m_SecondsRemaining = 10;
+        private int m_ShotsComplete = 0;
+        private DispatcherTimer m_GameTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+            m_GameTimer = new DispatcherTimer();
+            m_GameTimer.Interval = TimeSpan.FromSeconds(1);
+            m_GameTimer.Tick += GameTimer_TickHandler;
         }
 
-        private void LoadedHandler(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Enables dragging by windows contents
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MouseLeftButtonDownHandler(object sender, MouseButtonEventArgs e)
         {
-            this.MouseLeftButtonDown += (mSender, mE) =>
+            DragMove();
+        }
+
+        private void KeyUpHandler(object sender, KeyEventArgs e)
+        {
+            if (m_GameTimer.IsEnabled == false)
             {
-                DragMove();
-            };
+                m_GameTimer.Start();
+            }
+        }
+
+        private void GameTimer_TickHandler(object sender, EventArgs e)
+        {
+            m_SecondsRemaining--;
+
+            if (m_SecondsRemaining == -1)
+            {
+                m_SecondsRemaining = 59;
+                m_ShotsComplete++;
+            }
+
+            SecondsRemainingTextBlock.Text = m_SecondsRemaining.ToString();
+            ShotsCompleteTextBlock.Text = m_ShotsComplete.ToString();
         }
     }
 }
